44de11c839eedd39c9fe656cb0f05ac5a65f8668
TrinidadGabriel <gabrieltc@ciencias.unam.mx>
2026-02-19 22:25:15 -0600

    Practica1.hs Completada

--- src/Practica01.hs
+++ src/Practica01.hs
@@ -8,62 +8,103 @@ data Shape = Circle Float | --representa el radio
             Rectangle Float Float| --representa base y altura
             Triangle Float | --representa un lado
             Trapeze Float Float Float --representa base mayor, base menor y altura
-            deriving (Show)
+            deriving (Show, Eq)
 
 --Funcion que calcula el area de las figuras
 area :: Shape -> Float
-area = undefined
+area (Circle radio) = pi * radio * radio
+area (Square lado) = lado * lado
+area (Rectangle base altura) = base * altura
+area (Triangle lado) = (sqrt 3 / 4) * (lado * lado)
+area (Trapeze base_M base_m altura) = ((base_M + base_m) * altura ) / 2 
+
 
 --Funcion que calcula el perimetro de las figuras
 perimeter :: Shape -> Float
-perimeter = undefined
+perimeter (Circle radio) = 2 * radio * pi
+perimeter (Square lado) = 4 * lado
+perimeter (Rectangle base altura) = (2 * base) + (2 * altura)
+perimeter (Triangle lado) = (lado * 3)
+perimeter (Trapeze base_M base_m altura) = base_M + base_m + (2*lado)
+                                                where
+                                                baseTriangulo = (base_M - base_m) / 2
+                                                lado = sqrt ((baseTriangulo *baseTriangulo) + (altura * altura))
 
 
 --Ejercicio 2 (Les toca arreglar el sinonimo)
-type Point = Shape
+type Point = (Float, Float)
 
 -- Funcion para calcular la distancia entre dos puntos
 distance :: Point -> Point -> Float
-distance = undefined 
+distance (x1, y1) (x2, y2)= sqrt (((x2-x1)*(x2-x1)) + ((y2-y1)*(y2-y1)))
 
 --Funcion para calcular la distancia de un punto al origen
 from0 :: Point -> Float
-from0 = undefined
+from0 (x,y) = distance (x,y) (0,0)
 
---Ejercicio 3
-data Haskellium = Undefined
+--Ejercicio 3 
+data Haskellium = Haskellium {
+    name :: String,
+    lastName1 :: String,
+    lastName2 :: String,
+    location :: Point,
+    houseShape :: Shape
+} deriving(Show)
 
 --Funcion para regresar el hijo de dos Haskelliums dado su nombre
 son :: Haskellium -> Haskellium -> String -> Haskellium
-son = undefined
+son padre1 padre2 nombreHijo = Haskellium {
+    name = nombreHijo,
+    lastName1 = lastName1 padre1,
+    lastName2 = lastName1 padre2,
+    location = location padre1,
+    houseShape = houseShape padre1
+} 
 
 --Funcion para calcular las unidades para construir la casa de un Haskellium
 houseCost :: Haskellium -> Float
-houseCost = undefined
+houseCost padre = area (houseShape padre) + (perimeter (houseShape padre) * 2.5)
 
 --Funcion para calcular el tiempo que le toma a un Haskellium para llegar a su trabajo
 timeToWork :: Haskellium -> Float
-timeToWork = undefined
-
+timeToWork casa = distancia / velocidad 
+                    where
+                    distancia = from0 (location casa)
+                    velocidad = if distancia < 300 then 30 else 70
+                    
+                        
 --LISTAS Y FUNCIONES
 --Ejercicio 1
-palindromo :: String -> Bool
-palindromo = undefined
+palindromo :: (Eq a) => [a] -> Bool
+palindromo xs =  xs == invertir xs 
+                    where   
+                    invertir [] = []
+                    invertir (x:ys) = invertir ys ++ [x] 
+
 
 --Ejercicio 2
 myFoldr :: (a -> b -> b) -> b -> [a] -> b
-myFoldr = undefined
+myFoldr _ valor [] = valor
+myFoldr funcion valor (x:xs) = funcion x (myFoldr funcion valor xs) 
 
 --Ejercicio 3
 conjuntoPotencia :: [a] -> [[a]]
-conjuntoPotencia = undefined
+conjuntoPotencia [] = [[]]
+conjuntoPotencia (x:xs) = [ x:ys | ys <- resto ] ++ resto
+                            where 
+                            resto = conjuntoPotencia xs 
 
 --ARBOLES
 
 --Implementacion
 
-data OneTwoTree a = Undefinedd
+data OneTwoTree a = Void 
+                    | Node a (OneTwoTree a)
+                    | Branch a (OneTwoTree a) (OneTwoTree a)
+                      deriving (Show)
 
 --Ejercicio 2
 suma :: OneTwoTree Int -> Int
-suma = undefined
+suma Void = 0
+suma (Node valor hijo) = valor + suma hijo
+suma (Branch valor hijoIzq hijoDer ) =  valor + suma hijoIzq + suma hijoDer
